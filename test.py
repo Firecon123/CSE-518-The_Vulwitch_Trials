54473f464ec86ff3d76b663d097fea79dcbb480d
Firecon123 <137738719+Firecon123@users.noreply.github.com>
2025-10-14 23:40:26 -0400

    2.2

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-import torch
-print(torch.version.cuda)       # Should show 12.1
-print(torch.cuda.is_available()) # Should be True
-print(torch.cuda.device_count()) # Should be >= 1
-print(torch.cuda.get_device_name(0)) # Your GPU name
+def wrong_language():
+    print("I am wrong language")
